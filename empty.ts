80ad3681db1ac856f0f0c17ca7691c02a67fe2ab
Miguel Henley <Miguel Henley>
2017-02-05 21:48:01 -0200

    Alteracao

--- empty.ts
+++ empty.ts
@@ -1,10 +1,11 @@
+/* TESTE */
+
 class Greeter {
     constructor(public greeting: string) { }
     greet() {
-        return "<h1>" + this.greeting + "</h1>";
+        return "<h3>" + this.greeting + "</h3>";
     }
 };
 
-var greeter = new Greeter("Hello, world!");
-    
+var greeter = new Greeter("Hello, world!");    
 document.body.innerHTML = greeter.greet();
